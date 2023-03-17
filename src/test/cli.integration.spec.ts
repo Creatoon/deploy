af090421489fe6cb7eb47af91587552febb27fe9
Creatoon <yasharyan1099@gmail.com>
2023-03-17 23:20:14 +0530

    added logs

--- src/test/cli.integration.spec.ts
+++ src/test/cli.integration.spec.ts
@@ -153,8 +153,13 @@ describe('Integration CLI', function () {
 
 	// --token
 	it('Should be able to login using --token flag', async function () {
+		console.log(configFilePath());
 		const file = await loadFile(configFilePath());
+		console.log(file);
+		console.log('File conetent must be above');
 		const token = file.split('=')[1];
+		console.log(token);
+		console.log('Token must be above');
 
 		await clearCache();
 
